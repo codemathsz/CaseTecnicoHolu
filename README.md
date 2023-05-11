a642b99d37b4a51f248d12e327a7d2a139e13580
codemathsz <mo284891@gmail.com>
2023-05-11 16:11:15 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 align="center"> Case Técnico Holu Solar </h1>
+
+### START SCRIPT
+```powershell
+npm run dev
+```
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import { exit } from 'node:process';
+import readline from 'node:readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout  
+})
+
+function calculate(potentialTotal){
+
+    const structureType = "Laje"
+    const potential = 550;// watts
+    const dimensions = {length: 1.95, width: 1.1}
+
+    const totalPotentialWatts = potentialTotal * 1000;// convertendo
+    const amountPlates = Math.round(totalPotentialWatts / potential);
+    const inverters = Math.round(amountPlates/4);// 1 inversor par 4 placas
+    const lengthOfStructure = amountPlates * dimensions.length
+    const usefulArea = amountPlates * (dimensions.length * dimensions.width)
+
+    return{
+        amountPlates,
+        inverters,
+        potential,
+        lengthOfStructure,
+        usefulArea, 
+        structureType
+    }
+
+}
+
+rl.question("Qual a potencia total? ", (potentialTotal) =>{
+
+    const resultado = calculate(potentialTotal)
+
+    console.log("\nEstrutura usada: "+ resultado.structureType);
+    console.log("Quantidade de painéis: " + resultado.amountPlates);
+    console.log("Quantidade de inversores: " + resultado.inverters);
+    console.log("Potência do painel utilizado: " + resultado.potential + "W");
+    console.log("Comprimento de estrutura necessário: " + resultado.lengthOfStructure + "m");
+    console.log("Área útil necessária: " + resultado.usefulArea + "m²");
+    exit()
+})
+
+
+
+
+
+
